022ea6f6eae7a85255fca8ac68fc1adbb52fe371
Ruslan Khait <rusty6916@gmail.com>
2022-04-26 14:23:53 -0400

    commit tree 3580e0b1ab50571745b3b419223f7f6b8fe038f3

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+    def name=(this_dogs_name)
+        @name = this_dogs_name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(this_dog_breed)
+        @breed = this_dog_breed
+    end
+    
+    def breed
+        @breed
+    end
 end
+
+max = Dog.new
+max.name ="MAX"
+max.breed = "Yorkie"
+
+puts max.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    def name=(person_name)
+        @name = person_name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(person_job)
+        @job = person_job
+    end
+
+    def job
+        @job
+    end
 end
+
+rus = Person.new
+rus.name = "Ruski"
+rus.job = "Millionaire software/computer engineer"
+puts rus.job
